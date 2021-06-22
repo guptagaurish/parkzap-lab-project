b42ae9de0a7500ab45176fdd4e45b4b29d6130f2
Gaurish <gg160476@gmail.com>
2021-06-22 07:51:22 +0530

    deploy heroku

--- school/urls.py
+++ school/urls.py
@@ -18,12 +18,17 @@ from django.urls import path, include
 from student import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.register),
     path('existing-users/',views.existing),
     path('search/',views.search),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
 if settings.DEBUG:
