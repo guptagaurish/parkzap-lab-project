ac7d0e57fa4e0c3bccb0837fb27ac553afc6dd36
guptagaurish <67861590+guptagaurish@users.noreply.github.com>
2021-06-22 15:18:41 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -135,8 +135,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'simonlexus44@gmail.com'
-EMAIL_HOST_PASSWORD = 'lesi1234'
+EMAIL_HOST_USER = ' '
+EMAIL_HOST_PASSWORD = ' '
 EMAIL_PORT = 587
 
 django_heroku.settings(locals())
