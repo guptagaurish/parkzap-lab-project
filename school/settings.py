5d4404bde48f41f18cbc7f15659a658e1ce83faa
guptagaurish <67861590+guptagaurish@users.noreply.github.com>
2021-06-22 12:57:52 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,3 +139,4 @@ EMAIL_HOST_USER = 'simonlexus44@gmail.com'
 EMAIL_HOST_PASSWORD = 'lesi1234'
 EMAIL_PORT = 587
 
+django_heroku.settings(locals())
